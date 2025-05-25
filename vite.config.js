aab7a7296f99259593ef1b9adde602d705fd38be
nugiie <noviantonugroho4@gmail.com>
2025-05-25 21:48:20 +0700

    deploy yo netfly

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/project1-landingpage/',
+  base: process.env.NODE_ENV === 'production' && process.env.GITHUB_ACTIONS ? '/project1-landingpage/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
